4412712733cd7a165ac20e70815325c646505139
azizmousa <azizmousa1010@gmail.com>
2020-12-30 01:38:04 +0200

    plot the dataset

--- 2.regression/3.polynomial_regression/polynomial_regression.py
+++ 2.regression/3.polynomial_regression/polynomial_regression.py
@@ -6,3 +6,11 @@ dataset = pd.read_csv("Position_Salaries.csv")
 
 X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, 2].values
+
+plt.scatter(X, y, color="red")
+plt.plot(X, y, color="blue")
+plt.title("Position Salary Figure")
+plt.xlabel("Salary")
+plt.ylabel("Level")
+plt.show()
+
