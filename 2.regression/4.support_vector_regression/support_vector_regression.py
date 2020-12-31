2971c8f2fe7f0951de2020bc9cff8b1ac078c052
azizmousa <azizmousa1010@gmail.com>
2020-12-31 05:09:55 +0200

    create ploting module

--- 2.regression/4.support_vector_regression/support_vector_regression.py
+++ 2.regression/4.support_vector_regression/support_vector_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from Ploter import plot
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 dataset = pd.read_csv("Position_Salaries.csv")
@@ -8,20 +8,6 @@ X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, 2].values
 X = X.reshape(-1, 1)
 y = y.reshape(-1, 1)
-
-
-# plot function to draw data figures
-def plot(x_scat, y_scat, x_plt=None, y_plt=None, title="Figure", xlabel="X axis",
-         ylabel="Y axis", plot_color="blue", scat_color="red", draw_plot=True, figure=1):
-    plt.figure(figure)
-    plt.title(title)
-    plt.scatter(x_scat, y_scat, color=scat_color)
-    if draw_plot:
-        plt.plot(x_plt, y_plt, color=plot_color)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
-
 
 plot(X, y, X, y, xlabel="Level", ylabel="Salary", plot_color="green", draw_plot=True, figure=1)
 
