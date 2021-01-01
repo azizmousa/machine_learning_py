b6fade58d59389c63cd5afb2579f717029c8e050
azizmousa <azizmousa1010@gmail.com>
2021-01-01 04:15:40 +0200

    predict the training set

--- 2.regression/5.regression_tree/regression_tree.py
+++ 2.regression/5.regression_tree/regression_tree.py
@@ -11,8 +11,12 @@ y = y.reshape(-1, 1)
 
 plot(X, y, X, y, xlabel="Level", ylabel="Salary", title="Salary Level", plot_color="green", figure=1)
 
-reg_tree_model = tree.DecisionTreeRegressor()
+reg_tree_model = tree.DecisionTreeRegressor(random_state=0)
 reg_tree_model.fit(X, y)
 tree.plot_tree(reg_tree_model)
 # predict single value 4.5 >>>>>>>> 80000
-print(reg_tree_model.predict([[4.5]]))
+print(reg_tree_model.predict([[6.5]]))
+X_arn = np.arange(min(X), max(X), 0.1)
+X_arn = X_arn.reshape(-1, 1)
+plot(X, y, X_arn, reg_tree_model.predict(X_arn), xlabel="Level", ylabel="Salary",
+     title="Salary Level", plot_color="blue", figure=2)
