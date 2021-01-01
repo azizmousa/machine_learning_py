0c49d563c6a4d6e76e7d554f3ea46886fe633833
azizmousa <azizmousa1010@gmail.com>
2021-01-01 03:50:29 +0200

    implement regression tree model

---
+++ 2.regression/5.regression_tree/regression_tree.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from Ploter import plot
+from sklearn import tree
+data_path = "Position_Salaries.csv"
+dataset = pd.read_csv(data_path)
+X = dataset.iloc[:, 1].values
+y = dataset.iloc[:, 2].values
+X = X.reshape(-1, 1)
+y = y.reshape(-1, 1)
+
+plot(X, y, X, y, xlabel="Level", ylabel="Salary", title="Salary Level", plot_color="green", figure=1)
+
+reg_tree_model = tree.DecisionTreeRegressor()
+reg_tree_model.fit(X, y)
+tree.plot_tree(reg_tree_model)
