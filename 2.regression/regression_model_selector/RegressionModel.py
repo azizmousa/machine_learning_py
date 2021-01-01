7e4878884732d142cf388b946e5d0cb2c65107d6
azizmousa <azizmousa1010@gmail.com>
2021-01-01 21:23:00 +0200

    change the class initializer

--- 2.regression/regression_model_selector/RegressionModel.py
+++ 2.regression/regression_model_selector/RegressionModel.py
@@ -2,12 +2,19 @@ import numpy as np
 
 
 class RegressionModel:
+    _x_train = None
+    _y_train = None
+    _x_validation = None
+    _y_validation = None
 
-    def __inti__(self):
-        pass
+    def __inti__(self, x_train=None, y_train=None, x_validation=None, y_validation=None):
+        self._x_train = x_train
+        self._y_train = y_train
+        self._x_validation = x_validation
+        self._y_validation = y_validation
 
     # create_model function resposible for initialize the Regressin model and fit it with the data
-    def create_model(self, x_train=None, y_train=None, x_validation=None, y_validation=None):
+    def create_model(self):
         pass
 
     def evaluate_model(self):
