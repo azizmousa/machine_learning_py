8b2bd95451ae866542b3300a3eecfeea10ba8448
azizmousa <azizmousa1010@gmail.com>
2021-01-01 21:34:05 +0200

    add model state to the Parent class

--- 2.regression/regression_model_selector/RegressionModel.py
+++ 2.regression/regression_model_selector/RegressionModel.py
@@ -6,12 +6,14 @@ class RegressionModel:
     _y_train = None
     _x_validation = None
     _y_validation = None
+    _model = None
 
-    def __inti__(self, x_train=None, y_train=None, x_validation=None, y_validation=None):
+    def __inti__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None):
         self._x_train = x_train
         self._y_train = y_train
         self._x_validation = x_validation
         self._y_validation = y_validation
+        self._model = model
 
     # create_model function resposible for initialize the Regressin model and fit it with the data
     def create_model(self):
@@ -21,11 +23,11 @@ class RegressionModel:
         pass
 
     def get_model(self):
-        pass
+        return self._model
 
-    def set_model(self):
-        pass
-
+    def set_model(self, model):
+        self._model = model
+        
     # get the name of the model as string
     def to_string(self):
         pass
