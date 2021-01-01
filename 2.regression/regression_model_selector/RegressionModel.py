65325172adbad23dc5ada112c311b70b450c4a02
azizmousa <azizmousa1010@gmail.com>
2021-01-01 07:43:06 +0200

    create the abstract model

---
+++ 2.regression/regression_model_selector/RegressionModel.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class RegressionModel:
+
+    def __inti__(self):
+        pass
+
+    # create_model function resposible for initialize the Regressin model and fit it with the data
+    def create_model(self, x_train=None, y_train=None, x_validation=None, y_validation=None):
+        pass
+
+    def evaluate_model(self):
+        pass
+
+    def get_model(self):
+        pass
+
+    def set_model(self):
+        pass
+
+    # get the name of the model as string
+    def to_string(self):
+        pass
