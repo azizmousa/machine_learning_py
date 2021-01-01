dcd67a71c78f8e37c6f003cbcf6fc26f9c052648
azizmousa <azizmousa1010@gmail.com>
2021-01-02 01:25:28 +0200

    implement the create method

--- 2.regression/regression_model_selector/linear_regression_model.py
+++ 2.regression/regression_model_selector/linear_regression_model.py
@@ -1,16 +1,16 @@
-import regression_model
+from regression_model import RegressionModel
 from sklearn.linear_model import LinearRegression
 
 
-class LinearRegressionModel(regression_model.RegressionModel):
+class LinearRegressionModel(RegressionModel):
 
     def __init__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None):
+        if model is None:
+            model = LinearRegression()
         super().__init__(x_train, y_train, x_validation, y_validation, model)
 
     # create_model function resposible for initialize the Regressin model and fit it with the data
     def create_model(self):
-        if self._model is None:
-            super()._model = LinearRegression()
         self._model.fit(self._x_train, self._y_train)
 
     def evaluate_model(self):
@@ -19,3 +19,4 @@ class LinearRegressionModel(regression_model.RegressionModel):
     # get the name of the model as string
     def to_string(self):
         return "Linear Regreassion Model"
+
