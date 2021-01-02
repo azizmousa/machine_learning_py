418b1f7de4c776ff4e82a94c4b2eb0e5a417a425
azizmousa <azizmousa1010@gmail.com>
2021-01-02 04:10:42 +0200

    optimize svr model initializing

--- 2.regression/regression_model_selector/support_vector_regression.py
+++ 2.regression/regression_model_selector/support_vector_regression.py
@@ -1,27 +1,22 @@
 from regression_model import RegressionModel
 from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
 
 
 class SupportVectorRegression(RegressionModel):
 
-    _scaling_x = True
-    _scaling_y = True
+    _x_scaler = None
+    _y_scaler = None
 
-    def __init__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None, scaling_x=True,
-                 scaling_y=True):
-        self._scaling_x = scaling_x
-        self._scaling_y = scaling_y
+    def __init__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None, x_scaler=None,
+                 y_scaler=None):
+
+        self._x_scaler = x_scaler
+        self._y_scaler = y_scaler
 
         if model is None:
             model = SVR(kernel="rbf")
         super().__init__(x_train, y_train, x_validation, y_validation, model)
-
-    # create_model function resposible for initialize the Regressin model and fit it with the data
-    def create_model(self):
-        pass
-
-    def evaluate_model(self):
-        pass
 
     # get the name of the model as string
     def to_string(self):
