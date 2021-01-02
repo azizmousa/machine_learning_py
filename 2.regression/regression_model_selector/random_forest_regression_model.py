bf61213c783a0286344e663b23b1396c4668501e
azizmousa <azizmousa1010@gmail.com>
2021-01-02 05:01:24 +0200

    implement random forest regression model creator

--- 2.regression/regression_model_selector/random_forest_regression_model.py
+++ 2.regression/regression_model_selector/random_forest_regression_model.py
@@ -11,6 +11,12 @@ class RandomForestRegressionModel(RegressionModel):
             model = RandomForestRegressor(n_estimators=n_estimators, random_state=random_state)
         super().__init__(x_train, y_train, x_validation, y_validation, model)
 
+    # create_model function resposible for initialize the Regressin model and fit it with the data
+    def create_model(self):
+        print("Training Random Forest Regression Model ....")
+        self._model.fit(self._x_train, self._y_train.flatten())
+        print("Random Forest Regression Model Training is Finished.>")
+
     # get the name of the model as string
     def to_string(self):
         pass
