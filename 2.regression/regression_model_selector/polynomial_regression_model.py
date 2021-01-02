1c38f4744a5864cf0b0c06039d499c2a6f21d5cd
azizmousa <azizmousa1010@gmail.com>
2021-01-02 02:35:53 +0200

    implement the create method for the polynomial regression model

--- 2.regression/regression_model_selector/polynomial_regression_model.py
+++ 2.regression/regression_model_selector/polynomial_regression_model.py
@@ -5,25 +5,33 @@ from sklearn.linear_model import LinearRegression
 
 class LinearRegressionModel(RegressionModel):
 
-    _x_poly = []
     _degree_range = []
+    _degreed_model = {}
 
-    def __init__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None, degree_range=[]):
-        self._x_poly = []
-        self._degree_range = degree_range
-
+    def __init__(self, x_train=None, y_train=None, x_validation=None, y_validation=None, model=None, degree_range=None):
         if degree_range is None:
-            poly = PolynomialFeatures(degree=1)
-            self._x_poly.append(poly.fit_transform(x_train))
-        else:
-            self._degree_range = degree_range
-            for degree in degree_range:
-                poly = PolynomialFeatures(degree=degree)
-                self._x_poly.append(poly.fit_transform(x_train))
-
+            degree_range = []
+        self._degree_range = degree_range
+        self._degreed_model = {}
         if model is None:
             model = LinearRegression()
         super().__init__(x_train, y_train, x_validation, y_validation, model)
+
+        # create_model function resposible for initialize the Regressin model and fit it with the data
+
+    def create_model(self):
+        if self._degree_range is []:
+            print(f"Training Polynomial Regression Model of Degree [1] ....")
+            x_poly_train = PolynomialFeatures(degree=1)
+            self._model.fit(x_poly_train, self._y_train)
+        else:
+            for degree in self._degree_range:
+                print(f"Training Polynomial Regression Model of Degree [{degree}] ....")
+                x_poly_train = PolynomialFeatures(degree=degree)
+                model = LinearRegression()
+                model.fit(x_poly_train, self._y_train)
+                self._degreed_model.append({degree: model})
+                print(f"Training Polynomial Regression Model of Degree [{degree}] is Finished >>")
 
     # get the name of the model as string
     def to_string(self):
