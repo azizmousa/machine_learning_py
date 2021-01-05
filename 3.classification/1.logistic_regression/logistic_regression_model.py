da09f0c95ba5cdb50d0e6fd8938cf5f4ba9892fa
azizmousa <azizmousa1010@gmail.com>
2021-01-05 04:30:18 +0200

    calculate the confusion matrix and display it

--- 3.classification/1.logistic_regression/logistic_regression_model.py
+++ 3.classification/1.logistic_regression/logistic_regression_model.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 dataset = pd.read_csv("data.csv")
 X = dataset.iloc[:, :-1].values
@@ -18,3 +20,10 @@ x_train, x_test, y_tarin, y_test = train_test_split(X, y, test_size=0.2, random_
 classifier = LogisticRegression(random_state=0)
 classifier.fit(x_train, y_tarin.flatten())
 print(classifier.predict(x_scaler.fit_transform([[30, 87000]])))
+
+y_hat = classifier.predict(x_test)
+
+tn, fn, fp, tp = confusion_matrix(y_test, y_hat).ravel()
+
+plot_confusion_matrix(classifier, x_test, y_test)
+plt.show()
