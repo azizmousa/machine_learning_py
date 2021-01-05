da30e1616ca9dbee6027f5149ab202b7c6c5bc55
azizmousa <azizmousa1010@gmail.com>
2021-01-06 01:00:37 +0200

    fit the knn model with the dataset

--- 3.classification/2.knn/k_nearest_neighbor.py
+++ 3.classification/2.knn/k_nearest_neighbor.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.neighbors import KNeighborsClassifier
 
 dataset = pd.read_csv("data.csv")
 X = dataset.iloc[:, :-1].values
@@ -15,3 +15,8 @@ X = x_scaler.fit_transform(X)
 
 x_train, x_test, y_tarin, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+classifier = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2)
+classifier.fit(x_train, y_tarin.flatten())
+print(classifier.predict(x_scaler.fit_transform([[30, 87000]])))
+
+y_hat = classifier.predict(x_test)
