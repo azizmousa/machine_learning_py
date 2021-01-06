6ad4d78030d67dcb4dfcd1dd48df652c40b1ebc2
azizmousa <azizmousa1010@gmail.com>
2021-01-06 07:59:43 +0200

    fit the model to dataset

--- 3.classification/6.random_forest_classifier/random_forest_classifier.py
+++ 3.classification/6.random_forest_classifier/random_forest_classifier.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 dataset = pd.read_csv("data.csv")
 X = dataset.iloc[:, :-1].values
@@ -13,3 +14,8 @@ x_scaler = StandardScaler()
 X = x_scaler.fit_transform(X)
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+classifier = RandomForestClassifier(n_estimators=10, criterion='entropy')
+classifier.fit(x_train, y_train.flatten())
+
+print(classifier.predict(x_scaler.transform([[30, 87000]])))
