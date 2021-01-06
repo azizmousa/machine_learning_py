61a034623c295b5a1ad87e445c17c93fb8d56dbd
azizmousa <azizmousa1010@gmail.com>
2021-01-06 08:04:11 +0200

    calculate the confusion matix and accuracy

--- 3.classification/6.random_forest_classifier/random_forest_classifier.py
+++ 3.classification/6.random_forest_classifier/random_forest_classifier.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix, accuracy_score
 
 dataset = pd.read_csv("data.csv")
 X = dataset.iloc[:, :-1].values
@@ -15,7 +16,15 @@ X = x_scaler.fit_transform(X)
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-classifier = RandomForestClassifier(n_estimators=10, criterion='entropy')
+classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
 classifier.fit(x_train, y_train.flatten())
 
 print(classifier.predict(x_scaler.transform([[30, 87000]])))
+y_hat = classifier.predict(x_test)
+
+tn, fn, fp, tp = confusion_matrix(y_test, y_hat).ravel()
+print(f"tn: {tn}, fn: {fn}, fp: {fp}, tp: {tp}")
+plot_confusion_matrix(classifier, x_test, y_test)
+plt.show()
+
+print("accuracy: ", accuracy_score(y_test, y_hat))
