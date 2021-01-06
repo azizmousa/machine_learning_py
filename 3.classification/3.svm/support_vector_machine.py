3b9027b45c07da660b64a23c0b8afcd053ba80fd
azizmousa <azizmousa1010@gmail.com>
2021-01-06 02:20:52 +0200

    fit the svm model to the dataset

--- 3.classification/3.svm/support_vector_machine.py
+++ 3.classification/3.svm/support_vector_machine.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 
 dataset = pd.read_csv("data.csv")
@@ -14,4 +15,10 @@ x_scaler = StandardScaler()
 X = x_scaler.fit_transform(X)
 
 x_train, x_test, y_tarin, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+classifier = SVC(kernel="linear", random_state=0)
+classifier.fit(x_train, y_tarin.flatten())
+print(classifier.predict(x_scaler.fit_transform([[30, 87000]])))
+
+y_hat = classifier.predict(x_test)
 
